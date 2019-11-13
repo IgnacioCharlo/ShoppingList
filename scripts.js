7f1e5c1521e2295564b8618a0331ab7e3e83ba42
Sergio <sergiogarciacotos@gmail.com>
2019-11-13 15:37:39 +0100

    fixed disabling button when input is empty

--- scripts.js
+++ scripts.js
@@ -1,18 +1,30 @@
 var items = document.getElementById("items");
+var input = document.getElementById("imput");
+var inputbutton = document.getElementById("imputbutton");
+inputbutton.disabled = true;
 
 var n_items = 0;
 
-document.getElementById("imput").onkeypress = function(event){
+input.oninput = function(event){
+	text = document.getElementById("imput").value;
+	if(text.length==0)inputbutton.disabled=true;
+	else inputbutton.disabled=false;
+};
+
+
+input.onkeypress = function(event){
 	if (event.keyCode == 13){
 		addItem();
+		inputbutton.disabled=true;
 	}
-};
+}
 
 function updateTitle(){
 	title.innerHTML = "My shopping list (" + n_items + (n_items==1? " item":" items") + ")";
 }
 
 function addItem(){
+	inputbutton.disabled=true;
 	newItem = document.createElement("li");
 	name = document.getElementById("imput").value;
 	if(name!==""){
